8bbf74d8b94c3b06c7ad6d617b38f72d3b09e956
Ilovekerry <xxb2015@gmail.com>
2017-06-07 14:11:22 +0800

    new

--- js-task2/js/task2-5.js
+++ js-task2/js/task2-5.js
@@ -2,16 +2,36 @@ var a=sessionStorage.getItem("number1");//总人数
 var b=sessionStorage.getItem("number2");//杀手人数
 var c=sessionStorage.getItem("number3");//平民人数
 var bArr=JSON.parse(sessionStorage.getItem("list"));//顺序
-var dead=sessionStorage.getItem("s-role");//死掉的角色
-	console.log(a,b,c,bArr,dead);
-//下拉
-$(document).ready(function(){
-	$(".day1").click(function(){
-		$(".day1-panel").slideToggle(1000);
-	});
-//
-var order=0;
+var deadPerson=JSON.parse(sessionStorage.getItem("killList"));//死掉的角色数组
+var dead=deadPerson;
+var days=sessionStorage.getItem("s-days");//天数
+	console.log(a,b,c,bArr,dead,days,deadPerson);
+	console.log(dead);
 
+$(document).ready(function(){
+	//下拉列表
+	$(".day1").click(function(){
+		$(this).next("div").slideToggle(1000);
+	});
+	//关闭按钮清除存储
+	$(".header-right").click(function(){
+		sessionStorage.clear();
+		location.href="task2-1.html"
+	})
+//根据天数取得数组中的一个数的函数
+function diek(a){
+	a=deadPerson[a*2-2]
+	return a;
+}
+function diev(a){
+	a=deadPerson[a*2-1]
+	return a;
+}
+//定义游戏顺序
+var order=0;
+//神奇的函数
+	function dayOne(t){
+//有死亡人数，代表是杀人已经结束
 	if(dead!==null){
 		$(".panel-content-kill").click(function(){
 			alert("请按正确顺序进行游戏");
@@ -23,7 +43,9 @@ var order=0;
 		$(".panel-content-kill div div").css(
 				"border-right-color","grey");
 		//给出杀人结果
-		var x=dead-1+2;
+		var t=diek(t)-0+1;
+		console.log(t);
+		var x=t;
 		console.log(x);
 		$(".panel-content-kill").after(function(dead){
 			return "<div>"+x+"号被杀手杀死，真实身份是平民</div>"
@@ -31,7 +53,7 @@ var order=0;
 		$(".day1-panel-content >div:nth-child(2)").css({
 			"color":"black",
 			"font-size":"0.7rem",
-			"padding-left":"31px",
+			"text-indent":"31px",
 			"background-color":"white",
 			"justify-content":"flex-start",
 			"cursor":"default",
@@ -48,7 +70,7 @@ var order=0;
 					});
 				$(".panel-content-death div div").css(
 					"border-right-color","grey");
-				return order=1;
+				return order+=1;
 				}
 		
 
@@ -70,7 +92,7 @@ var order=0;
 				});
 			$(".panel-content-talk div div").css(
 				"border-right-color","grey");
-			return order=2;	
+			return order+=1;	
 					}
 					else{
 						alert("请按正确顺序进行游戏");
@@ -84,7 +106,7 @@ var order=0;
 			location.href="task2-6.html";
 			//表示已经进入投票环节
 			sessionStorage.setItem("vote",1);
-			return order=0;
+			return order+=1;
 		}
 		else{
 			alert("请按正确顺序进行游戏");
@@ -110,6 +132,42 @@ var order=0;
 		$(".panel-content-vote").click(function(){
 			alert("请按正确顺序进行游戏");
 		});
+
+	}
+	}
+
+	//如果是第一天
+if (days==null){
+	var t=1;
+	dayOne(t);//运行函数1
+}
+	//进入第二天
+	else if(days!==null){
+		console.log(days);
+		alert("欢迎来到第"+days+"天!");
+		$(".body-wrapper").append($(".body-wrapper>div:nth-child(-n+2)").clone(true));
+		//运行第一天
+		dayOne(1);
+		var t=days-1;
+		console.log(t);
+		var r=diev(t)-0+1;
+		console.log(r);
+		var x=r;
+		console.log(x);
+		var y=bArr[x-1];
+		console.log(y);
+		//加上投票结果
+		$(".panel-content-vote").after(function(dead){
+			return "<div>"+x+"号被投票投死，真实身份是"+y+"</div>"
+		});
+		$(".day1-panel-content >div:nth-child(6)").css({
+			"color":"black",
+			"font-size":"0.7rem",
+			"text-indent":"31px",
+			"background-color":"white",
+			"justify-content":"flex-start",
+			"cursor":"default",
+			});
 
 	}
 });
--- js-task2/js/task2-6.js
+++ js-task2/js/task2-6.js
@@ -2,6 +2,17 @@
 	var b=sessionStorage.getItem("number2");//杀手人数
 	var c=sessionStorage.getItem("number3");//平民人数
 	var bArr=JSON.parse(sessionStorage.getItem("list"));//顺序
+	var days=sessionStorage.getItem("s-days");//天数
+	var livePeople=[];//活着的数组
+	var deadPeople=[];//死掉的数组
+	var target=1000;	//定义被投死者的号数
+	var role=1000;	//定义被杀死者的号数
+	var deadPerson=JSON.parse(sessionStorage.getItem("killList"));//死掉的角色数组
+	//判断是否属于死掉的数组
+	function stillAlive(a){
+		var result=$.inArray(a,deadPerson);
+		return result;
+	}
 //总人数数组
 console.log(a,b,c);
 console.log(bArr);
@@ -24,28 +35,40 @@ $(document).ready(function(){
 };
 });
 
-//投票环节开始
+//开始
 $(document).ready(function(){
-//获得死者号数
-var dead=sessionStorage.getItem("s-role");//死掉的号数
-console.log("死者号数"+dead);
-//定义被投死者的号数
-var target;
-//定义生死
-var live=0;
+//点击关闭按钮关闭清除存储
+$(".header-right").click(function(){
+	sessionStorage.clear();
+	location.href="task2-1.html"
+})
+//更改背景颜色，代表死亡
+var people=$(".content >div");
+var y;
+if (deadPerson!=null) {
+	for(i=0;i<deadPerson.length;i++){
+		var y=deadPerson[i];
+		$(people[y]).find("div").css("background-color","#f8abab")
+		console.log(y);
+	}
+}
+console.log(people);
+
 //获得投票开始状态
 var start=sessionStorage.getItem("vote");
 console.log(start);
-//判断是否已进入投票环节，如果是，修改页面为投票页面
+//判断是否已进入投票环节
+//是
 if (start==1) {
+	//获得死者号数
+	console.log(deadPerson);
+	var dead=deadPerson[deadPerson.length-1];//死掉的号数
+	console.log("死者下标"+dead);
+//修改页面为投票页面
 	$(".header-middle").text("投票");
 	$(".nav-top").html("<div></div>发言讨论结束，大家请投票");
 	$(".nav-top div").addClass("nav-triangle");
 	$(".nav-bottom").text("点击得票数最多的人的头像");
-	//更改背景颜色，代表死亡
-	var people=$(".content >div div");
-	console.log(people);
-	$(people[dead]).css("background-color","#f8abab");
 	//选择目标时的效果
 	$(".content>div").bind("click",function(){
 		$(".content img").css("display","none");
@@ -55,17 +78,18 @@ if (start==1) {
 	$(".content>div").bind("click",function(){
 		target=$(this).index();
 		console.log(target);
-		console.log(dead);
-		if(target==dead-1){
+		if(stillAlive(target)!==-1){
 			alert("这个人已经死了，我们还是放过他吧");
 		};
+		return target;
 		});
-
+//按钮直接点击也能跳转
 	//点击按钮判断
 	$("#next").click(function(){
 		//投票结果分析
+
 		//判断是否投票了死者
-		if (target==dead-1) {
+		if (stillAlive(target)!==-1) {
 			alert("这个人已经死了，我们还是放过他吧");
 			location.href="#";
 		}
@@ -82,14 +106,36 @@ if (start==1) {
 		};
 		});
 	$("#next").click(function(){
-		console.log(b,c)
+		console.log(b,c);
+		console.log(target);
+		if (target==1000) {
+			alert("请选择一位活着的玩家进行投票")
+		}
+		else if (stillAlive(target)!==-1) {
+			location.href="#";
+		}
 		//判断是否游戏结束
-		if(b==0||b==c){
+		else if(b==0||b==c){
 			alert("游戏结束");//进入游戏结束页面
 		}
 		else{
 			//存储数据，进入第二天
-			sessionStorage.setItem("dead",target)
+			var deadPeople=deadPerson;
+			deadPeople.push(target);
+			console.log(deadPeople);
+			sessionStorage.setItem("killList",JSON.stringify(deadPeople));
+			console.log(target);
+			if (days==null) {
+				var days=2;
+				sessionStorage.setItem("s-days",days)
+				console.log(days);
+			}
+			else{
+				days+=1;
+				sessionStorage.setItem("s-days",days);
+				console.log(days);
+			}
+			location.href="task2-5.html";
 		}
 
 	});
@@ -106,19 +152,32 @@ else{
 		$(this).find("img").css("display","inline-flex");
 		//杀手选择目标
 		role=$(this).index();
+		if(bArr[role]=="杀手"){
+			alert("请选择一位平民");
+		}
 		console.log(role);
+		return role;
 		});
+	//点击确认按钮时的判断
 		$('#next').click(function(){
 			//如果目标是杀手自己
-			if(bArr[role]=="杀手"){
+			if(role==1000){
+				alert("请选择一位活着的平民")
+			}
+			else if(bArr[role]=="杀手"){
 				alert("请选择一位平民");
+			}
+			else if (stillAlive(role)!==-1) {
+				alert("请选择一位活着的玩家");
 			}
 			else if(b==c-1){
 				alert("游戏结束");//进入游戏结束页面
 			}
 			else{
 				//存储死者的号数
-				sessionStorage.setItem("s-role",role);
+				
+				deadPeople.push(role);
+				sessionStorage.setItem("killList",JSON.stringify(deadPeople));
 				console.log(role);
 				location.href="task2-5.html";
 			};
